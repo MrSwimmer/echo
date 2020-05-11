948057bdda76fe4a7870fad6347b2be8f6c716d3
seva <szhukov@icerockdev.com>
2020-05-11 21:49:34 +0700

    port

--- src/Application.kt
+++ src/Application.kt
@@ -1,6 +1,8 @@
 package ru.memeapp.echo
 
 import io.ktor.application.*
+import io.ktor.features.ContentNegotiation
+import io.ktor.gson.gson
 import io.ktor.http.ContentType
 import io.ktor.response.respondText
 import io.ktor.routing.Routing
@@ -19,6 +21,13 @@ fun main(args: Array<String>) {
 }
 
 fun Application.module() {
+    install(ContentNegotiation) {
+        gson {
+            setPrettyPrinting()
+            serializeNulls()
+        }
+    }
+
     install(Routing) {
         route("/") {
             get {
