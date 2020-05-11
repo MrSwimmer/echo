38a0722fe4c80f40e6f0d6f730c6e4c039fda2f4
seva <szhukov@icerockdev.com>
2020-05-11 21:40:20 +0700

    port

--- src/Application.kt
+++ src/Application.kt
@@ -3,15 +3,25 @@ package ru.memeapp.echo
 import io.ktor.application.*
 import io.ktor.http.ContentType
 import io.ktor.response.respondText
+import io.ktor.routing.Routing
 import io.ktor.routing.get
-import io.ktor.routing.routing
+import io.ktor.routing.route
 import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.EngineMain
 import io.ktor.server.netty.Netty
 
 fun main(args: Array<String>) {
+    EngineMain.main(args)
     val server = embeddedServer(Netty) {
-        routing {
-            get("/") {
+
+    }
+    server.start()
+}
+
+fun Application.module() {
+    install(Routing) {
+        route("/api") {
+            get("/lol") {
                 call.respondText("Hello World!", ContentType.Text.Plain)
             }
             get("/demo") {
@@ -19,5 +29,4 @@ fun main(args: Array<String>) {
             }
         }
     }
-    server.start()
 }
