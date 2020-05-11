d9b679a0692235fb97392b0250b71e5454e2dddc
seva <szhukov@icerockdev.com>
2020-05-11 18:27:14 +0700

    Initial commit

---
+++ src/Application.kt
@@ -0,0 +1,20 @@
+package ru.memeapp.echo
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+
+fun main(args: Array<String>) {
+    val server = embeddedServer(Netty) {
+        routing {
+            get("/") {
+                call.respondText("Лол")
+            }
+        }
+    }
+    server.start(wait = true)
+}
+
