5c93f0f1d8df3c46d3b2098856ac9a16e00c7b4e
seva <szhukov@icerockdev.com>
2020-05-12 02:11:49 +0700

    echo

--- src/model/Button.kt
+++ src/model/Button.kt
@@ -2,7 +2,6 @@ package model
 
 data class Button(
     val hide: Boolean?,
-    val payload: PayloadX?,
     val title: String?,
     val url: String?
 )
--- src/model/Meta.kt
+++ src/model/Meta.kt
@@ -2,7 +2,6 @@ package model
 
 data class Meta(
     val client_id: String,
-    val interfaces: Interfaces,
     val locale: String,
     val timezone: String
 )
