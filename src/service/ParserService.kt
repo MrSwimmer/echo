d71ffcbb0ace06af00279dfb0fcfd6dabb9b45db
Seva <mrswimmerlab@gmail.com>
2020-06-01 01:19:00 +0700

    id + delay

--- src/service/ParserService.kt
+++ src/service/ParserService.kt
@@ -4,10 +4,7 @@ import com.google.gson.Gson
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.apache.Apache
 import io.ktor.client.request.get
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.*
 import ru.memeapp.echo.repository.JokeRepository
 import ru.memebattle.model.vk.model.VKResponse
 
@@ -30,7 +27,7 @@ class ParserService(
 
                 val client = HttpClient(Apache)
 
-                val groupIds = listOf("", "")
+                val groupIds = listOf("149279263")
                 groupIds.forEach { groupId ->
                     val response =
                         client.get<String>("https://api.vk.com/method/wall.get?owner_id=-$groupId&access_token=$vkToken&v=5.103&extended=1&count=100")
@@ -50,6 +47,8 @@ class ParserService(
                         }
                     }
                 }
+
+                delay(60000 * 30)
             }
         }
     }
